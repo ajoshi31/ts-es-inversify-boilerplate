ef17909de0dd09d8fc5732eadf0094eb06cf5b3c
Atul Joshi <atjoshi.net@gmail.com>
2022-12-13 21:50:17 +0530

    no empty interface disabled

--- src/module/user/repository/iuser.repository.interaface.ts
+++ src/module/user/repository/iuser.repository.interaface.ts
@@ -1,6 +1,4 @@
 import { IBaseRepository } from '@shared-infra/persistence/mongo/IBaseRepository';
 import { IUser } from '@user-module/model/IUser';
 
-export interface IUserRepository extends IBaseRepository<IUser> {
-  super();
-}
+export interface IUserRepository extends IBaseRepository<IUser> {}
