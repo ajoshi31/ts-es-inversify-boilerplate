8e6476cc682a217e11a5f4b8ca6b6d33ca6aa400
Atul Joshi <atjoshi.net@gmail.com>
2022-12-19 21:58:16 +0530

    Added cors, compression and helmet

--- src/shared/infra/http/middleware/global/basic.ts
+++ src/shared/infra/http/middleware/global/basic.ts
@@ -1,11 +1,18 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
 
 const basicMiddleware = (app: express.Application) => {
   app.enable('trust proxy');
   app.use(express.json());
+  app.set('etag', false);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.set('etag', false);
+  app.use(cors());
+  app.use(helmet());
+  app.use(compression());
   app.use(
     bodyParser.urlencoded({
       limit: '50mb',
