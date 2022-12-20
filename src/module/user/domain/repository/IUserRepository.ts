1340883a8687db8b80369688cc1108788eebe112
Atul Joshi <atjoshi.net@gmail.com>
2022-12-20 11:49:15 +0530

    Comment added

--- src/module/user/domain/repository/IUserRepository.ts
+++ src/module/user/domain/repository/IUserRepository.ts
@@ -1,7 +1,7 @@
 import { IBaseRepository } from '@shared-infra/persistence/mongo/IBaseRepository';
 import { IUserModel } from '@shared-infra/persistence/mongo/models/UserModel';
 
-// Here we should be passing the ORM model entity
+// Here we are be passing the ORM model entity instead of Entitybeacuse of base repo pattern
 export interface IUserRepository extends IBaseRepository<IUserModel> {
   // If any other implementation are happening here we can pass the direct domain entity class
 }
