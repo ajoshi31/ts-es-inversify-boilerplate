5fdf5295156551c3368299aadc7a103d04725859
Nishit Gupta <nishit.gupta6708@gmail.com>
2023-05-25 16:54:44 +0530

    update: morgan

--- src/shared/infra/http/middleware/global/Morgan.ts
+++ src/shared/infra/http/middleware/global/Morgan.ts
@@ -1,14 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import { logger } from '@core/logger/Logger';
+import { AppUtils } from '@core/utils/AppUtils';
+import config from '@config/config';
+import * as dotenv from 'dotenv';
+import { JwtPayload } from 'jsonwebtoken';
+dotenv.config();
+
+const MORGAN_TEXT =
+  ':remote-addr <-> :method <-> :url <-> :user <-> :res[content-length] <-> :response-time ms';
 
 const winstonStream = {
   write: (message: string) => {
     logger.info(message);
   }
 };
+
+morgan.token('user', (req: Request, res: Response): any => {
+  const token = req.header('accessToken') as string;
+  const user = new AppUtils().verifyToken(
+    token,
+    config.ACCESS_TOKEN_KEY as string
+  ) as JwtPayload;
+  return user.isRight() ? user.value.userId : '';
+});
+
 const morganMiddleware = (app: express.Application) => {
-  app.use(morgan('combined', { stream: winstonStream }));
+  app.use(morgan(MORGAN_TEXT, { stream: winstonStream }));
 };
 
 export default morganMiddleware;
